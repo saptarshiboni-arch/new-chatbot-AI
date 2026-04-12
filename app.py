c3fbe43f0612b55038e2cb076f59dc0e1683e083
saptarshiboni-arch <saptarshiboni@gmail.com>
2026-04-12 22:07:52 +0530

    temporary medibot fix

--- app.py
+++ app.py
@@ -89,7 +89,6 @@ def history():
     return render_template("history.html", chats=data)
 
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
